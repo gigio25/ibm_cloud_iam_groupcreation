622dcc74dc2fb07eb63b5cbf2dec1973906f1529
Geovani Miccoli <geovani.miccoli@gmail.com>
2025-05-06 13:55:36 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
 resource "ibm_iam_access_group" "accgrp" {
- name = "test_gigio"
-}
-resource "ibm_iam_access_group_policy" "policy" {
- access_group_id = ibm_iam_access_group.accgrp.id
- roles           = ["Viewer"]
+ name        = "test"
+ description = "New access group"
 }
