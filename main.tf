9979f5776dcfb9e8c51ebfb502d1afbbf90044b0
Geovani de Mello Miccoli <gigio@ibm.com>
2025-05-06 14:24:38 -0300

    alterando pra editor

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "ibm_iam_access_group" "accgrp" {
 }
 resource "ibm_iam_access_group_policy" "policy" {
   access_group_id = ibm_iam_access_group.accgrp.id
-  roles           = ["Viewer"]
+  roles           = ["Editor"]
 }
